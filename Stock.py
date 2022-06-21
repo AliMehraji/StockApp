5ee34cb9a87cd1fc6e9c11b7534f293cb7c28fb0
Ali mehr <a.mehraji75@gmail.com>
2022-06-22 00:49:27 +0430

    new: Add Decorator and fix some other

--- Stock.py
+++ Stock.py
@@ -1,21 +1,37 @@
 """This is a Stock List Program"""
+from typing import final
+
+
 stock = {}
 
 
 def _help():
-    """This is Help Menu"""
+    """Help Menu"""
+
     print("""
           H     : Show this Menu
           New   : Add New stuff
           Del   : Delete stuff
           Edt   : Edit stuff
           Info  : Info about one stuff
-          Show  : Show all stuffs
+          Show  : Show List
+          Done  : Exit from the Specific part
           Exit  : Exit Or [Ctrl ^ c]
 
           *** Notice! Sensitive to Lower or Upper Case Letters ***
 
           """)
+
+
+def _Checker(fun):
+    """Stock Emptiness Checker"""
+
+    def wrapper(*args, **kwargs):
+        if not stock:
+            print("There is nothing in stock")
+        else:
+            return fun()
+    return wrapper
 
 
 def new_stuff():
@@ -30,84 +46,71 @@ def new_stuff():
         print()
 
 
+@_Checker
 def show_all_stuffs():
-    """Showing all Stuffs list"""
+    """Shows whole Stuffs in list"""
 
-    if not stock:
-        print("There is not anything in stock")
-    else:
+    print("--------------------------")
+    for code in stock:
+        print(f"Code ... ...{code}")
+        for key, val in stock.get(code).items():
+            print(f"{key} ... ... {val}")
         print("--------------------------")
-        for code in stock:
-            print(f"Code ... ...{code}")
-            for key, val in stock.get(code).items():
-                print(f"{key} ... ... {val}")
-            print("--------------------------")
 
 
+@_Checker
 def show_one_stuff():
-    """Showing a specific stuff in list by its code """
+    """Shows a specific stuff in list by its code """
 
-    if not stock:
-        print("There is not anything in stock")
-    else:
-        print()
-        code = input("Stuff's code ?  :  ")
-        show = stock.get(code, "it is not in list")
-        print(f"\nCode .... ...{code}")
-        for key, val in show.items():
-            print(f"{key} .... ... {val}")
+    print()
+    code = input("Stuff's code ?  :  ")
+    show = stock.get(code, "it is not in list")
+    print(f"\nCode .... ...{code}")
+    for key, val in show.items():
+        print(f"{key} .... ... {val}")
 
 
+@_Checker
 def edit_stuff():
-    
-    if not stock:
-        print("There is not anything in stock")
 
-    else :
-        print(""" 
-              [1]  : for Editing Stuff's code
-              [2]  : for Editing Stuff's name or existence 
-              Done : Exit editing
-              Show : Show Stock List
-          """)
+    print(""" 
+            [1]  : for Editing Stuff's code
+            [2]  : for Editing Stuff's name or existence 
+        """)
 
-        while (user_input := input("choose [1 Or 2] to Edit : ")) != "Done":
+    while (user_input := input("choose [1 Or 2] to Edit : ")) != "Done":
 
-            # Edit Stuff's Code
-            if user_input == "1":
+        # Edit Stuff's Code
+        if user_input == "1":
 
-                print("Editing Stuff's Code! ")
-                code1 = input("previous Code? : ")
-                code2 = input("New Code? : ")
+            print("Editing Stuff's Code! ")
+            code1 = input("previous Code? : ")
+            code2 = input("New Code? : ")
 
-                stock[code2] = stock[code1]
-                del stock[code1]
+            stock[code2] = stock[code1]
+            del stock[code1]
 
-            # Edit Stuff's Name or existence
-            elif user_input == "2":
+        # Edit Stuff's Name or existence
+        elif user_input == "2":
 
-                print("Editing Stuff's Name or existence! ")
-                code = input("Stuff's Code? : ")
-                name = input("Stuff's New Name ? : ")
-                existence = input("Stuff's New Existence : ")
+            print("Editing Stuff's Name or existence! ")
+            code = input("Stuff's Code? : ")
+            name = input("Stuff's New Name ? : ")
+            existence = input("Stuff's New Existence : ")
 
-                stock[code]["Name"] = name
-                stock[code]["Exist"] = existence
+            stock[code]["Name"] = name
+            stock[code]["Exist"] = existence
 
-            elif user_input == "Show":
-                show_all_stuffs()
+        elif user_input == "Show":
+            show_all_stuffs()
 
-
+@_Checker
 def del_stuff():
-    """delete stuff from list"""
+    """Delete stuff from list"""
 
-    code = input(
-        """which stuff you want to delete ?\n[type "Show" for what is in list]\ncode: """)
-    if code == "Show":
-        show_all_stuffs()
-    else:
-        if not stock:
-            print("There is not anything in stock")
+    while (code:=input("Deleting which stuff ? code : ")) != "Done":
+        if code == "Show":
+            show_all_stuffs()
         else:
             if code in stock:
                 del stock[code]
@@ -117,7 +120,7 @@ def del_stuff():
 
 
 def _main():
-    """main program to interact with User"""
+    """Main program to interact with User"""
     user = "Command [H for Help] : "
     print()
     while (user_input := input(user)) != "Exit":
@@ -141,4 +144,7 @@ if __name__ == "__main__":
         _main()
 
     except KeyboardInterrupt:
+        print("Exited!")
+    
+    finally:
         print("\nHave a good Day. Bye!\n")
